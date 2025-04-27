244d77c60cac8c61bbaf35874c0b89357752b1b7
smabdurrehmanshah <smabdurrahmanshah@gmail.com>
2025-04-27 16:36:45 +0500

    add the todo functionality

--- js/home.js
+++ js/home.js
@@ -7,6 +7,39 @@ else {
   userName.textContent = currentUser.fullName;
 }
 
+const form = document.getElementById('form');
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+renderTodos();
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const todo = {
+    todo: event.target.todoInput.value,
+    reference: currentUser.email
+  };
+
+  todos.push(todo);
+  localStorage.setItem('todos', JSON.stringify(todos));
+  renderTodos();
+  event.target.reset();
+});
+
+function renderTodos() {
+  const ulElement = document.querySelector('.list-group');
+  ulElement.innerHTML = "";
+
+  const currentTodos = todos.filter(item => currentUser.email === item.reference);
+
+  for(let i=0; i<currentTodos.length; i++) {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = currentTodos[i].todo;
+    ulElement.appendChild(li);
+  }
+}
+
+
 const logoutBtn = document.getElementById('logout-btn');
 
 logoutBtn.addEventListener('click', (event) => {
